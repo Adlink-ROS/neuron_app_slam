fc4d7dd5f8a6b0b50d283027a955511c76702b3f
ChenYing Kuo <evshary@gmail.com>
2020-11-24 17:04:24 +0800

    Use correct way to assign absolute path for map.

--- src/napp_slam/launch/map_saver_cli.launch.py
+++ src/napp_slam/launch/map_saver_cli.launch.py
@@ -2,6 +2,7 @@ import os
 
 from launch import LaunchDescription
 from launch_ros.actions.node import Node
+from pathlib import Path
 
 def generate_launch_description():
 
@@ -9,7 +10,7 @@ def generate_launch_description():
         package='nav2_map_server',
         executable='map_saver_cli',
         parameters=[{'save_map_timeout': 10000}],
-        arguments=['-f', '$HOME/neuron_app_slam/yourmap'],
+        arguments=['-f', str(Path.home())+'/neuron_app_slam/yourmap'],
         output='screen'
     )
 
